dd792134edf675e504e854a92c971e6fa4b10ddb
hernad <hernad@bring.out.ba>
2017-04-26 15:16:27 +0200

    check if vagrant box list | grep ARTACK/debian-jessie

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -2,6 +2,9 @@
 
 cd vagrant
 
-vagrant box add ARTACK/debian-jessie https://atlas.hashicorp.com/ARTACK/boxes/debian-jessie
+if ! vagrant box list | grep ARTACK/debian-jessie
+then
+   vagrant box add ARTACK/debian-jessie https://atlas.hashicorp.com/ARTACK/boxes/debian-jessie
+fi
 vagrant init ARTACK/debian-jessie
 vagrant up
